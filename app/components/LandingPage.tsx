0c0eda2f5126aa87b1da7c81a7d0f33a56a5105a
Giorgiod91 <giorgio.dettmar@gmx.de>
2024-10-29 12:16:17 +0100

    basic setup for the frontend part

---
+++ app/components/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+function LandingPage({}: Props) {
+  return (
+    <div className="flex ">
+      <div className="w-1/2 ">
+        <h1>Create Your own folder structure </h1>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
